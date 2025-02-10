445ef0c4e88137f3725a97a5bec2af8beec971f3
spydermnky <peterkhomchenk00@gmail.com>
2025-02-10 18:38:52 -0500

    Loading DAT Files Assignment Complete
    - Added text file representation of DAT file
    - Added screenshot of DAT file working in Tile World

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -2,7 +2,12 @@ import cc_dat_utils
 
 #Part 1
 input_dat_file = "data/pfgd_test.dat"
+load_files = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file)
+string_representation = str(load_files)
 
+file = open("data/pfgd_test.txt","w")
+file.write(string_representation)
+file.close()
 
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
 #print the resulting data
